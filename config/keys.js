f7be8784e43e27c935e913fd4f964df243d9a13f
ytyeason <tong.yang@mail.mcgill.ca>
2021-02-28 23:38:58 -0800

    add travis yaml file and code changes to setup env variables

--- config/keys.js
+++ config/keys.js
@@ -5,10 +5,15 @@
  * - In Development: Pull the keys values defined in the .env root of the project for development.
  */
 
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGODB_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  port: process.env.PORT
+if (process.env.NODE_ENV === "ci") {
+  module.exports = require("./ci");
+} else {
+  module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGODB_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    redisUrl: process.env.REDIS_URL,
+    port: process.env.PORT,
+  };
 }
--- index.js
+++ index.js
@@ -40,8 +40,8 @@ app.use(passport.session());
 setupAuthRoutes(app);
 setupBlogRoutes(app);
 
-// Production-only settings
-if (["production"].includes(process.env.NODE_ENV)) {
+// Production & CI-only settings
+if (["production", "ci"].includes(process.env.NODE_ENV)) {
   app.use(express.static("client/build"));
   app.get("*", (req, res) =>
     res.sendFile(path.resolve("client", "build", "index.html"))
